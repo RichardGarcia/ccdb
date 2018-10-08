1efe4e6c699d17519948b3e562560d8d3ff8cff3
Richard Garcia <richardgarciaph@gmail.com>
2018-10-08 18:03:38 +1300

    update Styled components & CSS grid, add top menu

--- src/App.js
+++ src/App.js
@@ -3,25 +3,36 @@ import styled from 'styled-components'
 
 import './App.css';
 
-const CustomElement = styled.div `
-  font-size: 30px;
+const MainContainer = styled.div `
+  display: grid;
+  grid-template-columns: 75% auto auto;
+  grid-column-gap: 20px;
+  padding: 20px;
 `
-const BlueElement = CustomElement.extend `
 
+const LogoContainer = styled.div `
+  font-size: 2.5em;
 `
-
+const NavLink = styled.div `
+  font-size: 1.5em;
+  align-self: center;
+  justify-self: center;
+`
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <CustomElement>
-          <p>Welcome to CCDB</p>
-        </CustomElement>
-        <BlueElement>
-          <p>Welcome to CCDB</p>
-        </BlueElement>
-      </React.Fragment>
+      <MainContainer>
+        <LogoContainer>
+          Welcome to CcDb
+        </LogoContainer>
+        <NavLink>
+          Dashboard
+        </NavLink>
+        <NavLink>
+          Settings
+        </NavLink>
+      </MainContainer>
     )
   }
 }
