8df32c325496ebfd5293711814796b47591d7a9e
Richard Garcia <richardgarciaph@gmail.com>
2018-10-10 11:51:14 +1300

    add click event to change page..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
 import './App.css';
 
-const MainContainer = styled.div `
+const NavContainer = styled.div `
   display: grid;
   grid-template-columns: 75% auto auto;
   grid-column-gap: 20px;
@@ -14,24 +14,51 @@ const LogoContainer = styled.div `
   font-size: 2.5em;
 `
 const NavLink = styled.div `
+  cursor: pointer;
   font-size: 1.5em;
   align-self: center;
   justify-self: center;
+  ${props =>props.active && css`
+    text-shadow: 0px 0px 60px #fff;
+    color: blue;
+  `}
+
+`
+const MainContainer = styled.div `
+  border: 1px solid red;
+`
+const MainContentContainer = styled.div `
+border: 1px solid red;
+padding: 20px;
+font-size: 1em;
 `
 
 class App extends Component {
+
+  state = {
+    activePageTitle: 'dashboard'
+  }
+
+  displayDashboard = () => this.state.activePageTitle === 'dashboard'
+  displaySettings = () => this.state.activePageTitle === 'settings'
+
   render() {
     return (
       <MainContainer>
-        <LogoContainer>
-          Welcome to CcDb
-        </LogoContainer>
-        <NavLink>
-          Dashboard
-        </NavLink>
-        <NavLink>
-          Settings
-        </NavLink>
+        <NavContainer>
+          <LogoContainer>
+            Welcome to CcDb
+          </LogoContainer>
+          <NavLink onClick={() => { this.setState({ activePageTitle: 'dashboard' }) }} active={this.displayDashboard()}>
+            Dashboard
+          </NavLink>
+          <NavLink onClick={() => { this.setState({ activePageTitle: 'settings' }) }} active={this.displaySettings()}>
+            Settings
+          </NavLink>
+        </NavContainer>
+        <MainContentContainer>
+          <h2>{this.state.activePageTitle}</h2>
+        </MainContentContainer>
       </MainContainer>
     )
   }
